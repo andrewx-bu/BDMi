a0ffbd593ced134734b86cda176d7cab62ff8c31
Bruce Casillas <bruce.casillas562@gmail.com>
2025-03-29 04:19:03 -0400

    Added the app and started working on database connections. Got firebase to connect and make basic add/read functions

---
+++ app/src/main/java/com/example/bdmi/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.bdmi
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bdmi.ui.theme.BDMiTheme
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+
+private const val TAG = "BDMi"
+
+class MainActivity : ComponentActivity() {
+    private lateinit var db: FirebaseFirestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        try {
+//            db = initializeFirestore()
+//            Log.d("MainActivity", "Firestore initialized")
+//        } catch (e: Exception) {
+//            Log.w("MainActivity", "Error initializing Firestore", e)
+//        }
+//        addData(db)
+//        readData(db)
+
+        enableEdgeToEdge()
+        setContent {
+            BDMiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BDMiTheme {
+        Greeting("Android")
+    }
+}
