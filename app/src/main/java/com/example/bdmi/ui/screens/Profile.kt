c024837ce8089b936fbe27d26721d67f69d3c1d9
andrewx-bu <andrewx@bu.edu>
2025-04-15 12:02:24 -0400

    animation for profile

--- app/src/main/java/com/example/bdmi/ui/screens/Profile.kt
+++ app/src/main/java/com/example/bdmi/ui/screens/Profile.kt
@@ -5,6 +5,11 @@ import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
@@ -29,10 +34,18 @@ import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -146,6 +159,35 @@ fun ProfileScreen(
 
 @Composable
 fun ProfilePicture(profileImageUrl: String, onEditClick: () -> Unit) {
+    val value by rememberInfiniteTransition().animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = LinearEasing
+            )
+        )
+    )
+
+    // Animation provided by https://rodrigomartind.medium.com/the-art-of-small-animations-in-android-with-jetpack-compose-566caa94deba
+    val colors = listOf(
+        Color(0xFF405DE6),
+        Color(0xFFC13584),
+        Color(0xFFFD1D1D),
+        Color(0xFFFFDC80)
+    )
+    val gradientBrush by remember {
+        mutableStateOf(
+            Brush.horizontalGradient(
+                colors = colors,
+                startX = -10.0f,
+                endX = 400.0f,
+                tileMode = TileMode.Repeated
+            )
+        )
+    }
+
     Box(
         modifier = Modifier.size(300.dp),
         contentAlignment = Alignment.Center
@@ -160,6 +202,16 @@ fun ProfilePicture(profileImageUrl: String, onEditClick: () -> Unit) {
             modifier = Modifier
                 .size(250.dp)
                 .clip(CircleShape)
+        )
+        Box(modifier = Modifier
+            .drawBehind {
+                rotate(value) {
+                    drawCircle(
+                        gradientBrush, style = Stroke(width = 12.dp.value)
+                    )
+                }
+            }
+            .size(250.dp)
         )
 
         // Edit Icon in Bottom-Right
