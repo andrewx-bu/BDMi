1966bf2e3185734fa8978bccb3070f974ec58eb8
Bruce Casillas <bruce.casillas562@gmail.com>
2025-04-25 01:17:04 -0400

    Created ReviewsRepository.kt, which has all the database functions. Will add review support for user information next.

---
+++ app/src/main/java/com/example/bdmi/data/repositories/ReviewRepository.kt
@@ -0,0 +1,317 @@
+package com.example.bdmi.data.repositories
+
+import android.util.Log
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.Source
+import javax.inject.Inject
+
+// Constants
+private const val TAG = "ReviewRepository"
+private const val REVIEWS_COLLECTION = "reviews"
+private const val RATINGS_COLLECTION = "ratings"
+private const val MOVIES_COLLECTION = "movies"
+
+class ReviewRepository @Inject constructor(
+    private val db: FirebaseFirestore
+) {
+    /*
+     * Creates a new review for a movie using transactions
+     * Updates the movies total review count
+     * Calls setRating to update the rating meta data
+     */
+    fun createReview(userId: String, movieId: Int, review: Review, onComplete: (Boolean) -> Unit) {
+        val dbFunction = "CreateReview"
+        Log.d("$TAG$dbFunction", "Creating review for user $userId and movie $movieId")
+
+        checkIfMovieExists(movieId) {
+            val movieDoc = db.collection(MOVIES_COLLECTION).document(movieId.toString())
+            val reviewDoc = movieDoc.collection(REVIEWS_COLLECTION).document(userId)
+
+            db.runTransaction { transaction ->
+                val snapshot = transaction.get(movieDoc)
+                val reviewSnapshot = transaction.get(reviewDoc)
+
+                // Gets new values for review count and average meta data
+                val reviewCount = snapshot.getDouble("reviewCount")?: 0.0
+                val newReviewCount = if (reviewSnapshot.exists()) reviewCount else reviewCount + 1
+
+                transaction.update(movieDoc, "reviewCount", newReviewCount)
+                transaction.set(reviewDoc, review)
+            }.addOnSuccessListener {
+                Log.d("$TAG$dbFunction", "Review created successfully")
+                setRating(userId, movieId, review.rating)
+
+                onComplete(true)
+            }.addOnFailureListener { e ->
+                Log.w("$TAG$dbFunction", "Error creating review", e)
+
+                onComplete(false)
+            }
+        }
+    }
+
+    // Deletes review and updates the movies total review count
+    fun deleteReview(userId: String, movieId: Int, onComplete: (Boolean) -> Unit) {
+        val dbFunction = "DeleteReview"
+        Log.d("$TAG$dbFunction", "Deleting review")
+
+        checkIfMovieExists(movieId) {
+            val movieDoc = db.collection(MOVIES_COLLECTION).document(movieId.toString())
+            val reviewDoc = movieDoc.collection(REVIEWS_COLLECTION).document(userId)
+
+            db.runTransaction { transaction ->
+                val snapshot = transaction.get(movieDoc)
+
+                // Gets new values for review count and average meta data
+                val reviewCount = snapshot.getDouble("reviewCount")?: 0.0
+                val newReviewCount = reviewCount - 1
+                transaction.update(movieDoc, "reviewCount", newReviewCount)
+                transaction.delete(reviewDoc)
+            }.addOnSuccessListener {
+                Log.d("$TAG$dbFunction", "Review deleted successfully")
+            }.addOnFailureListener { e ->
+                Log.w("$TAG$dbFunction", "Error deleting review", e)
+            }
+        }
+
+    }
+
+    /*
+     * Retrieves all reviews for a movie with pagination
+     * If lastVisible is null, it retrieves the first page of reviews
+     * Otherwise, it retrieves the next page of reviews
+     * Pagination code written by ChatGPT
+     */
+    fun getReviews(
+        movieId: Int,
+        lastVisible: DocumentSnapshot? = null,
+        pageSize: Int = 20,
+        onComplete: (List<Review>, DocumentSnapshot?) -> Unit
+    ) {
+        val dbFunction = "GetReviews"
+        Log.d("$TAG$dbFunction", "Getting reviews for movie $movieId")
+
+        checkIfMovieExists(movieId) {
+            val reviewQuery = db.collection(MOVIES_COLLECTION)
+                .document(movieId.toString())
+                .collection(REVIEWS_COLLECTION)
+                .orderBy("timestamp", Query.Direction.DESCENDING)
+                .limit(pageSize.toLong())
+
+            val paginatedQuery = if (lastVisible != null) {
+                reviewQuery.startAfter(lastVisible)
+            } else {
+                reviewQuery
+            }
+
+            paginatedQuery.get().addOnSuccessListener { querySnapshot ->
+                val reviews = querySnapshot.toObjects(Review::class.java)
+                val newLastVisible = querySnapshot.documents.lastOrNull()
+                onComplete(reviews, newLastVisible)
+            }
+        }
+    }
+
+    // Unsure what to use this function for right now, but it could be useful in the future
+    fun getReview(userId: String, movieId: Int, onComplete: (Review?) -> Unit) {
+        val dbFunction = "GetReview"
+        Log.d("$TAG$dbFunction", "Getting review for user $userId and movie $movieId")
+
+        checkIfMovieExists(movieId) {
+            val reviewDoc = db.collection(MOVIES_COLLECTION).document(movieId.toString())
+                .collection(REVIEWS_COLLECTION).document(userId)
+
+            reviewDoc.get().addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    val review = documentSnapshot.toObject(Review::class.java)
+                    onComplete(review)
+                }
+            }
+        }
+    }
+
+    /*
+     * Whenever a user rates a movie, this function is called
+     * Updates the rating meta data with new values that depend if a user has rated before
+     */
+    fun setRating(userId: String, movieId: Int, rating: Float) {
+        val dbFunction = "SetRating"
+        Log.d("$TAG$dbFunction", "Setting rating for user $userId and movie $movieId")
+
+        checkIfMovieExists(movieId) {
+            val movieDoc = db.collection(MOVIES_COLLECTION).document(movieId.toString())
+            val ratingDoc = movieDoc.collection(RATINGS_COLLECTION).document(userId)
+
+            db.runTransaction { transaction ->
+                val snapshot = transaction.get(movieDoc)
+                val ratingSnapshot = transaction.get(ratingDoc)
+                val updates = mutableMapOf<String, Any>()
+
+                // Gets new values for rating count and average meta data
+                val existingUserRating = ratingSnapshot.getDouble("rating")
+                val ratingCount = snapshot.getDouble("ratingCount")?: 0.0
+                val ratingSum = snapshot.getDouble("ratingSum") ?: 0.0
+
+                // If the user has already rated the movie, update the rating count and sum
+                val newRatingSum = if (existingUserRating != null)
+                    ratingSum - existingUserRating + rating
+                else ratingSum + rating
+
+                val newRatingCount = if (existingUserRating != null)
+                    ratingCount
+                else ratingCount + 1
+                val newAverageRating = newRatingSum / newRatingCount
+
+                updates["ratingCount"] = newRatingCount
+                updates["ratingSum"] = newRatingSum
+                updates["averageRating"] = newAverageRating
+
+                // Gets new values for rating breakdown
+                val ratingBreakdown = snapshot.get("ratingBreakdown") as Map<*, *>
+                val newRatingBreakdown = ratingBreakdown.toMutableMap()
+                val ratingString = rating.toString()
+                val currentCount = newRatingBreakdown[ratingString]?.toString()?.toInt() ?: 0
+                newRatingBreakdown[ratingString] = currentCount + 1
+                if (existingUserRating != null) {
+                    val oldBucket = existingUserRating.toString()
+                    newRatingBreakdown[oldBucket] = (newRatingBreakdown[oldBucket]?.toString()?.toInt() ?: 1) - 1
+                }
+
+                updates["ratingBreakdown"] = newRatingBreakdown
+
+                // Updates the movie meta data
+                transaction.update(movieDoc, updates)
+
+                // Sets the rating for the user in the movie's ratings collection
+                transaction.set(ratingDoc, mapOf("rating" to rating))
+            }.addOnSuccessListener {
+                Log.d("$TAG$dbFunction", "Rating set successfully")
+            }.addOnFailureListener { e ->
+                Log.w("$TAG$dbFunction", "Error setting rating", e)
+            }
+        }
+    }
+
+    /*
+     * Whenever a user removes a rating, this function is called
+     * Updates the rating meta data with new values that depend if a user has rated before
+     */
+    fun removeRating(userId: String, movieId: Int) {
+        val dbFunction = "RemoveRating"
+        Log.d("$TAG$dbFunction", "Removing rating for user $userId and movie $movieId")
+
+        checkIfMovieExists(movieId) {
+            val movieDoc = db.collection(MOVIES_COLLECTION).document(movieId.toString())
+            val ratingDoc = movieDoc.collection(RATINGS_COLLECTION).document(userId)
+
+            db.runTransaction { transaction ->
+                val snapshot = transaction.get(movieDoc)
+                val ratingSnapshot = transaction.get(ratingDoc)
+                val updates = mutableMapOf<String, Any>()
+
+                // Gets new values for rating count and average meta data
+                val existingUserRating = ratingSnapshot.getDouble("rating")
+                val ratingCount = snapshot.getDouble("ratingCount")?: 0.0
+                val ratingSum = snapshot.getDouble("ratingSum") ?: 0.0
+
+                val newRatingSum = if (existingUserRating != null)
+                    ratingSum - existingUserRating
+                else ratingSum
+
+                val newRatingCount = if (existingUserRating != null)
+                    ratingCount - 1
+                else ratingCount
+                val newAverageRating = newRatingSum / newRatingCount
+
+                updates["ratingCount"] = newRatingCount
+                updates["ratingSum"] = newRatingSum
+                updates["averageRating"] = newAverageRating
+
+                // Gets new values for rating breakdown
+                val ratingBreakdown = snapshot.get("ratingBreakdown") as Map<*, *>
+                val newRatingBreakdown = ratingBreakdown.toMutableMap()
+                val ratingString = existingUserRating.toString()
+                val currentCount = newRatingBreakdown[ratingString]?.toString()?.toInt() ?: 0
+                newRatingBreakdown[ratingString] = currentCount - 1
+
+                updates["ratingBreakdown"] = newRatingBreakdown
+
+                // Updates the movie meta data
+                transaction.update(movieDoc, updates)
+
+                // Deletes the rating for the user in the movie's ratings collection
+                transaction.delete(ratingDoc)
+            }.addOnSuccessListener {
+                Log.d("$TAG$dbFunction", "Rating removed successfully")
+            }.addOnFailureListener { e ->
+                Log.w("$TAG$dbFunction", "Error removing rating", e)
+            }
+        }
+
+    }
+
+    /*
+     * Called when a movie detail page is opened
+     * Checks if the movie exists in the local cached version of the database
+     * If it doesn't, it adds it to the database with default values
+     */
+    private fun checkIfMovieExists(movieId: Int, onComplete: () -> Unit) {
+        val dbFunction = "CheckIfMovieExists"
+
+        val movieDoc = db.collection(MOVIES_COLLECTION).document(movieId.toString())
+
+        movieDoc.get(Source.CACHE).addOnSuccessListener { documentSnapshot ->
+            if (!documentSnapshot.exists()) {
+                val movieData = mapOf(
+                    "reviewCount" to 0,
+                    "averageRating" to 0.0,
+                    "ratingCount" to 0,
+                    "ratingSum" to 0.0,
+                    "ratingBreakdown" to mapOf(
+                        ".5" to 0,
+                        "1" to 0,
+                        "1.5" to 0,
+                        "2" to 0,
+                        "2.5" to 0,
+                        "3" to 0,
+                        "3.5" to 0,
+                        "4" to 0,
+                        "4.5" to 0,
+                        "5" to 0
+                    ),
+                )
+                Log.d("$TAG$dbFunction", "Movie does not exist in local cache, adding it")
+                movieDoc.set(movieData).addOnSuccessListener { onComplete() }
+            } else {
+                onComplete()
+                Log.d("$TAG$dbFunction", "Movie already exists in local cache")
+            }
+        }
+    }
+}
+
+// Uses userId as documentId
+data class Review(
+    val userName: String,
+    val userProfilePicture: String,
+    val reviewTitle: String,
+    val reviewText: String,
+    val rating: Float,
+    val spoiler: Boolean = false,
+    val timestamp: Timestamp = Timestamp.now()
+)
+
+// Example of using pagination in view model
+/*
+var lastSnapshot: DocumentSnapshot? = null
+
+fun loadNextPage() {
+    getReviews(movieId, lastVisible = lastSnapshot, pageSize = 10) { reviews, newLast ->
+        reviewList.addAll(reviews)
+        lastSnapshot = newLast
+    }
+}
+ */
